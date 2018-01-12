fcbe631b5f8b41bf198eaaba105e780f5dbc5ecf
Anders Busted-Janum <abja@sdc.dk>
2018-01-12 23:47:22 +0100

    Added Solution file

---
+++ Grumpy.Common/Grumpy.Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Grumpy.Common")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Grumpy.Common")]
+[assembly: AssemblyCopyright("Copyright © Anders Busted-Janum 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1b3cf45d-09b7-4d21-b112-857b7dd8387a")]
+[assembly: AssemblyVersion("1.0.*")]
